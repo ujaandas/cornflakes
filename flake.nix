a8808f129dab429c01880ffa28cdbf3db59d2141
Ujaan Das <ujaandas03@gmail.com>
2024-12-11 22:27:21 +0800

    refactor into darwin folder

--- flake.nix
+++ flake.nix
@@ -9,35 +9,13 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
+    # Import the external configuration
+    configurationPath = ./darwin/configuration.nix;
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+    darwinConfigurations."ooj" = nix-darwin.lib.darwinSystem {
+      # Reference the external configuration file
+      modules = [ configurationPath ];
     };
   };
 }
