3b7009e286034a6b97cb36c5986422fae2714bab
Ujaan Das <ujaandas03@gmail.com>
2025-02-04 17:33:51 +0530

    move nix setup

--- users/ooj/home.nix
+++ users/ooj/home.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  imports = [ ../common.nix ];
+  imports = [ ../common.nix ./zsh/default.nix ];
 
   home.username = "ooj";
   home.homeDirectory = "/Users/ooj";
@@ -12,34 +12,5 @@
     userEmail = "ujaandas03@gmail.com";
   };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableVteIntegration = true;
-    syntaxHighlighting.enable = true;
-    autocd = true;
-    autosuggestion.enable = true;
-    oh-my-zsh = {
-      enable = true;
-      theme = "robbyrussell";
-      plugins = ["git" "sudo" "docker"];
-    };
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k-config";
-        src = ./zsh;
-        file = "p10k.zsh";
-      }
-    ];
-    shellAliases = {
-      ll = "ls -l";
-      nixup = "nix run nix-darwin --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.config/nix";
-    };
-    history.size = 10000;
-  };
+  programs.zsh = import ./zsh/default.nix { inherit pkgs; };
 }
---
+++ users/ooj/zsh/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  enable = true;
+  enableCompletion = true;
+  enableVteIntegration = true;
+  syntaxHighlighting.enable = true;
+  autocd = true;
+  autosuggestion.enable = true;
+  oh-my-zsh = {
+    enable = true;
+    theme = "robbyrussell";
+    plugins = ["git" "sudo"];
+  };
+  plugins = [
+    {
+      name = "powerlevel10k";
+      src = pkgs.zsh-powerlevel10k;
+      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    }
+    {
+      name = "powerlevel10k-config";
+      src = ./zsh;
+      file = "p10k.zsh";
+    }
+  ];
+  shellAliases = {
+    ll = "ls -l";
+    nixup = "nix run nix-darwin --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.config/nix";
+  };
+  history.size = 10000;
+}
