d101f04e3deddb73d1414863ef1c75e8498f4e2b
Ujaan Das <ujaandas03@gmail.com>
2025-02-05 13:41:18 +0530

    install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Install Nix
+# Install Nix using the determinate installer (https://github.com/DeterminateSystems/nix-installer)
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
+  sh -s -- install
 
 # Install homebrew (needed for GUI apps on macos)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Source config
+nix run nix-darwin --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake ~/.config/nix
